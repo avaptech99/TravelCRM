22da2cb7a871aeddf4d99a59fb50f187d51b2d2c
Anmol498 <anmoldeepsingh3886@gmail.com>
2026-05-01 17:33:57 +0530

    restore: revert to 2.0 folder structure and remove root files as requested

--- travel-crm-backend/src/routes/externalRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createExternalLead } from '../controllers/externalController';
-
-const router = express.Router();
-
-router.post('/lead', createExternalLead);
-
-export default router;
